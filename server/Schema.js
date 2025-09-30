2a485d1bfe08f1522476856facde1027fb61e284
Rudraksha <rudrakshakumbhkar@gmail.com>
2025-09-30 21:47:17 +0530

    5th commit

--- server/Schema.js
+++ server/Schema.js
@@ -1,12 +1,9 @@
 const Joi = require("joi");
 
 module.exports.pinSchema = Joi.object({
-    listing : Joi.object({
+    pin: Joi.object({
         title: Joi.string().required(),
         destination: Joi.string().required(),
-        image: Joi.valid('image/jpeg', 'image/png', 'image/gif').required(),
-        userId: Joi.string(),
-        category: Joi.string(),
-        
+        category: Joi.string().allow('').optional(), // Allow category to be optional
     }).required()
 });
--- server/models/pin.js
+++ server/models/pin.js
@@ -10,13 +10,12 @@ const pinSchema = new Schema({
     type: String,
   },
   image: {
-    url: String, // Store URL from Cloudinary
-    filename: String // Store filename from Cloudinary
+    url: String, // Correctly defined as an object property
+    filename: String 
   },
   category: {
     type: String,
   },
-  // This is the correct way to link to the User model
   postedBy: {
     type: Schema.Types.ObjectId,
     ref: 'User'
@@ -28,5 +27,4 @@ const pinSchema = new Schema({
 });
 
 const Pin = mongoose.model('Pin', pinSchema);
-
 module.exports = Pin;
