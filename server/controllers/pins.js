ea791bdb52ef36abd944c14f10a51d0a74e29cfe
Rudraksha <rudrakshakumbhkar@gmail.com>
2025-10-01 19:11:12 +0530

    6th commit

--- server/controllers/pins.js
+++ server/controllers/pins.js
@@ -12,21 +12,35 @@ module.exports.renderNewForm = (req, res) => {
 
 
 
-//Create Pin
+module.exports.index = async (req, res) => {
+    const allPins = await Pin.find({}).populate("postedBy");
+    res.render("pins/index.ejs", { allPins });
+};
+
+module.exports.renderNewForm = (req, res) => {
+    res.render("pins/new.ejs");
+};
+
 module.exports.createPin = async (req, res, next) => {
     const newPin = new Pin(req.body.pin);
+
+    // Ensure req.file exists before trying to access its properties
+    if (!req.file) {
+        req.flash("error", "Image file is required.");
+        return res.redirect("/pins/new");
+    }
+
     newPin.image = {
         url: req.file.path,
         filename: req.file.filename
     };
-
     newPin.postedBy = req.user._id;
 
     await newPin.save();
+
     req.flash("success", "New Pin created!");
     res.redirect("/pins");
 };
-
 
 module.exports.showPin = async (req, res) => {
     let { id } = req.params;
