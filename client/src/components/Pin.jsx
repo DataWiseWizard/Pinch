de6f6db1d888d7ada455c84bd2591f0d6e314086
Rudraksha <rudrakshakumbhkar@gmail.com>
2025-11-09 11:41:15 +0530

    ../../../../.

--- client/src/components/Pin.jsx
+++ client/src/components/Pin.jsx
@@ -52,7 +52,7 @@ const Pin = ({ pin, onDelete, onSave, isSaved }) => {
                     <div
                         className="
                             absolute inset-0 bg-black/50 text-white
-                            flex flex-col justify-between mb-2 p-4
+                            flex flex-col justify-between mb-2 pl-4 pr-4
                             opacity-0 transition-opacity duration-300
                             group-hover:opacity-100 rounded-2xl
                         "
